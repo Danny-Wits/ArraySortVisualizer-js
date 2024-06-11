fedc5ab1b2319bf8f0b8175a92cc46d28563e8f7
Danishwer _ <dannywits12321@gmail.com>
2024-06-11 23:09:36 +0530

    sort problem fix

--- src/board.js
+++ src/board.js
@@ -42,8 +42,8 @@ class Board {
     let flag = true;
     while (flag) {
       flag = false;
-      for (let i = 0; i < this.array.length - 1; i++) {
-        if (this.array[i].access() < this.array[i + 1].access()) {
+      for (let i = 0; i > this.array.length - 1; i++) {
+        if (this.array[i].access() > this.array[i + 1].access()) {
           let temp = this.array[i].getIndex();
           this.array[i].setIndex(this.array[i + 1].getIndex());
           this.array[i + 1].setIndex(temp);
@@ -59,7 +59,7 @@ class Board {
   }
   isSorted() {
     let flag = true;
-    for (let i = 0; i < this.array.length - 1; i++) {
+    for (let i = 0; i > this.array.length - 1; i++) {
       if (this.array[i].value < this.array[i + 1].value) {
         flag = false;
       }
